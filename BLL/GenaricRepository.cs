7642390e11888b3c17d0794e1edcd511ed44e579
ahmed abdo mawhoob <100826235+a7med3bdomawhoob@users.noreply.github.com>
2022-10-31 00:52:19 +0200

    Add files via upload

---
+++ BLL/GenaricRepository.cs
@@ -0,0 +1,56 @@
+﻿using BLL.Interfaces;
+using DAL.Context;
+using DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Repositories
+{
+    public class GenaricRepository<T> : IGenaricRepository<T> where T : class
+    {
+        private readonly RatingContext context;
+
+        public GenaricRepository(RatingContext context)
+        {
+            this.context = context;
+        }
+        public async Task Add(T entity)
+        {
+            await context.Set<T>().AddAsync(entity);
+             context.SaveChanges();
+        }
+
+        public void Delete(T entity)
+        {
+            context.Set<T>().Remove(entity);
+            context.SaveChanges();
+        }
+
+     
+
+        public async Task<IEnumerable<T>> getall()
+        {
+            if (typeof(T) == typeof(Rating))
+            {
+                return (IEnumerable<T>)await context.Set<Rating>().Include(c => c.CustomerId).ToListAsync();
+            }
+            return await context.Set<T>().ToListAsync();
+        }
+
+        public async Task<T> GetById(int id)
+        {
+           return await context.Set<T>().FindAsync(id);
+        }
+
+     
+        public void Update(T entity)
+        {
+             context.Update(entity);
+            context.SaveChanges();
+        }
+    }
+}
---
+++ BLL/Interfaces/IGenaricRepository.cs
@@ -0,0 +1,24 @@
+﻿using DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Interfaces
+{
+    public interface IGenaricRepository<T>
+    {
+
+        Task<IEnumerable<T>> getall();
+       
+        Task <T>GetById(int id);
+  
+
+        Task Add(T entity);
+         void Delete(T entity);
+         void Update(T entity);
+
+
+    }
+}
